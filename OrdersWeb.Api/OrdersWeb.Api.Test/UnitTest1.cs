883775981c18f259544dd1ab0d38455f06acc5a3
cvigueras <carlosj.vigueras@gmail.com>
2024-08-29 16:08:00 +0200

    chore: init project.

---
+++ OrdersWeb.Api/OrdersWeb.Api.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace OrdersWeb.Api.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
